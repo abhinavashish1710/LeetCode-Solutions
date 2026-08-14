856c0e36efccecc60f82e299ed26e2ea7ba05311
Abhinav Ashish <abhinavashish1106@gmail.com>
2026-08-14 15:13:04 +0530

    Solve LeetCode #103 - Binary Tree Zigzag Level Order Traversal

---
+++ Trees/0103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level;
+
+            while (n--) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            if (!leftToRight)
+                reverse(level.begin(), level.end());
+
+            ans.push_back(level);
+            leftToRight = !leftToRight;
+        }
+
+        return ans;
+    }
+};
