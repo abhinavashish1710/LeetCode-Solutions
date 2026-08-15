1ef2c651bba465ec83fc3fa02e4006806cddc61d
Abhinav Ashish <abhinavashish1106@gmail.com>
2026-08-15 23:46:51 +0530

    Solve LeetCode #108 - Convert Sorted Array to Binary Search Tree

---
+++ Trees/108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty()) return nullptr;
+
+        int mid = nums.size() / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid + 1, nums.end());
+
+        root->left = sortedArrayToBST(left);
+        root->right = sortedArrayToBST(right);
+
+        return root;
+    }
+};
