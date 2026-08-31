543c0ae18685acce8d69665b719ebc66d5d7c2ab
Abhinav Ashish <abhinavashish1106@gmail.com>
2026-08-31 22:21:01 +0530

    Solve LeetCode #131 - Palindrome Partitioning

---
+++ Backtracking/0131-Palindrome-Partitioning/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> path;
+
+    bool isPal(string &s, int l, int r) {
+        while (l < r)
+            if (s[l++] != s[r--]) return false;
+        return true;
+    }
+
+    void dfs(string &s, int i) {
+        if (i == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int j = i; j < s.size(); j++) {
+            if (isPal(s, i, j)) {
+                path.push_back(s.substr(i, j - i + 1));
+                dfs(s, j + 1);
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        dfs(s, 0);
+        return ans;
+    }
+};
