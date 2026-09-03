f4496dd21f8b55127a1172d4dac13757330c0483
Abhinav Ashish <abhinavashish1106@gmail.com>
2026-09-03 23:27:45 +0530

    Solve LeetCode #147 - Insertion Sort List
    Implement insertion sort for a linked list using a dummy node.

---
+++ LinkedList/0147-Insertion-Sort-List/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(0);
+
+        while(head) {
+            ListNode* cur=head;
+            head=head->next;
+
+            ListNode* p=&dummy;
+            while(p->next && p->next->val<cur->val)
+                p=p->next;
+
+            cur->next=p->next;
+            p->next=cur;
+        }
+
+        return dummy.next;
+    }
+};
